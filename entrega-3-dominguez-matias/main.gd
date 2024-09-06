641b13cfc63da33a2e0eb5af4f1eeaf6cfff11b2
Matias Dominguez UNQ <90357058+MatiasDominguezUNQ@users.noreply.github.com>
2024-09-06 16:35:17 -0300

    Delete entrega-3-dominguez-matias directory

--- entrega-3-dominguez-matias/main.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	$Player.set_projectile_container(self)
-	$Player.set_camera($Player/Camera2D)
